3a71028294d6b433a3ce3c773eb8aad42d952961
kevinwi911 <kevin.wi@doverunner.com>
2025-05-30 08:56:49 +0900

    docs : wiseplay 상수 설명 주석 보완
    1. 기능추가
    - 없음
    
    2. 버그수정
    - 없음
    
    3. 개발환경변경
    - 없음
    
    Issue : #6 #7 #8
    Jira : MD-3773, MD-4041, MD-4046, MD-4048

--- doverunner/config/wiseplay/security_level.py
+++ doverunner/config/wiseplay/security_level.py
@@ -1,9 +1,9 @@
 # for @security_policy @wiseplay @security_level
 SECURITY_LEVEL = (1, 2, 3)
 
-SW_LEVEL_1 = SECURITY_LEVEL[0]  # 'LEVEL_150',
-HW_LEVEL_2 = SECURITY_LEVEL[1]  # 'LEVEL_2000'
-ENHANCED_HW_LEVEL_3 = SECURITY_LEVEL[2]  # 'LEVEL_3000'
+SW_LEVEL_1 = SECURITY_LEVEL[0]  # 'SW_LEVEL_1',
+HW_LEVEL_2 = SECURITY_LEVEL[1]  # 'HW_LEVEL_2'
+ENHANCED_HW_LEVEL_3 = SECURITY_LEVEL[2]  # 'ENHANCED_HW_LEVEL_3'
 
 def check(security_level) -> bool:
     return security_level in SECURITY_LEVEL
